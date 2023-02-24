62e6c538dda655f79af68dab81bcba8c03376ddf
saitek <saitek300@gmail.com>
2023-02-23 19:10:14 -0800

    fixing typo

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from "apollo/client";
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
  mutation login($email: String!, $password: String!) {
@@ -41,3 +41,4 @@ export const REMOVE_BOOK = gql`
   }
  }
 `;
+
