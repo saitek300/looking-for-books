b2b5271331e2507b217dcd6b2fe5e51fa9f407d9
saitek <saitek300@gmail.com>
2023-02-26 17:40:56 -0800

    removed route require add client index build

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,7 +20,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs,resolvers) => {
   await server.start();
